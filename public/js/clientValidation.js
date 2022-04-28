c512bc67e0491164de2bcaf2aec7c14d217b8524
Murat <muratulu2001@gmail.com>
2022-04-28 19:18:12 -0400

    Standardized thrown errors in clientValidation.js
    Standardized thrown errors in clientValidation.js to be uniform with
    rest of project (now throws errors as strings, not objects). Also
    created functions for each form to check for and throw errors to be
    used in try/catch block.

--- public/js/clientValidation.js
+++ public/js/clientValidation.js
@@ -1,5 +1,6 @@
-//Client-side form validation for all login/signup pages
+//Client-side form validation
 (function () {
+    //Instantiates JS variables to corresponding DOM element with matching id
     const loginForm = document.getElementById("login-form");
     const businessLoginForm = document.getElementById("businessLogin");
     const businessSignUpForm = document.getElementById("businessSignUp");
@@ -14,25 +15,48 @@
         }
     }
 
+    //Error checking for all login form inputs, throws error if an input is invalid
+    function loginFormErrorCheck (businessEmail, email, password) {
+        if (!businessEmail || !email || !password) throw "Please complete all fields";
+        if (!checkEmail(businessEmail) && !checkEmail(email)) throw "Email addresses provided are invalid";
+        if (!checkEmail(businessEmail)) throw "Business email address provided is invalid";
+        if (!checkEmail(email)) throw "Email provided address is invalid";
+        if (password.length < 8) throw "Password must be at least 8 characters";
+    }
+
+    //Error checking for all business login form inputs, throws error if an input is invalid
+    function businessLoginFormErrorCheck (email, password) {
+        if (!email || !password) throw "Please complete all fields";
+        if (!checkEmail(email)) throw "Email address provided is invalid";
+        if (password.length < 8) throw "Password must be at least 8 characters";
+    }
+
+    //Error checking for all business signup form inputs, throws error if an input is invalid
+    function businessSignUpFormErrorCheck (businessName, email, password, confirmPassword, address, city, state, zip, phoneNumber, about) {
+        if (!businessName || !email || !password || !confirmPassword || !address ||
+            !city || !state || !zip || !phoneNumber || !about) throw "Please complete all fields";
+        if (!checkEmail(email)) throw "Email address provided is invalid";
+        if (password.length < 8) throw "Password must be at least 8 characters";
+        if (password != confirmPassword) throw "Passwords do not match";
+        if (!states.includes(state.toUpperCase())) throw "State must be entered in 2 letter format";
+        if (!zipRE.test(zip)) throw "Zip code must be entered in 5 digit format";
+        if (!phoneRE.test(phone)) throw "Phone number must be entered in 10 digit format";
+    }
+
     //Checks for input errors on "user/login" form and displays error msg to user if error detected
     if(loginForm){
         loginForm.addEventListener("submit", event => {
             const errorDiv = document.getElementById("errorDiv");
-            errorDiv.hidden = false;
+            errorDiv.hidden = true;
 
             try {
                 const businessEmail = document.getElementById("businessEmail").value.trim();
                 const email = document.getElementById("email").value.trim();
                 const password = document.getElementById("password").value.trim();
-                if (!businessEmail || !email || !password) throw new Error ("Please complete all fields");
-                if (!checkEmail(businessEmail) && !checkEmail(email)) throw new Error ("Email addresses provided are invalid");
-                if (!checkEmail(businessEmail)) throw new Error ("Business email address provided is invalid");
-                if (!checkEmail(email)) throw new Error ("Email provided address is invalid");
-                if (password.length < 8) throw new Error ("Password must be at least 8 characters");
+                loginFormErrorCheck(businessEmail, email, password);
             } catch (e) {
                 event.preventDefault();
-                let errorMsg = e.toString().slice(7);
-                errorDiv.innerHTML = errorMsg;
+                errorDiv.innerHTML = e;
                 errorDiv.hidden = false;
             }
             
@@ -43,19 +67,15 @@
     if(businessLoginForm){
         businessLoginForm.addEventListener("submit", event => {
             const errorDiv = document.getElementById("errorDiv");
-            errorDiv.hidden = false;
+            errorDiv.hidden = true;
 
             try {
                 const email = document.getElementById("email").value.trim();
                 const password = document.getElementById("password").value.trim();
-                if (!email || !password) throw new Error ("Please complete all fields");
-                if (!checkEmail(email)) throw new Error ("Email address provided is invalid");
-                if (password.length < 8) throw new Error ("Password must be at least 8 characters");
-                
+                businessLoginFormErrorCheck(email, password);         
             } catch (e) {
                 event.preventDefault();
-                let errorMsg = e.toString().slice(7);
-                errorDiv.innerHTML = errorMsg;
+                errorDiv.innerHTML = e;
                 errorDiv.hidden = false;
             }
             
@@ -66,7 +86,7 @@
     if(businessSignUpForm){
         businessSignUpForm.addEventListener("submit", event => {
             const errorDiv = document.getElementById("errorDiv");
-            errorDiv.hidden = false;
+            errorDiv.hidden = true;
 
             let states = ['AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY'];
             let zipRE = /^\d{5}$/;
@@ -83,19 +103,10 @@
                 const zip = document.getElementById("zip").value.trim();
                 const phoneNumber = document.getElementById("phoneNumber").value.trim();
                 const about = document.getElementById("about").value.trim();
-                if (!businessName || !email || !password || !confirmPassword || !address ||
-                    !city || !state || !zip || !phoneNumber || !about) throw new Error ("Please complete all fields");
-                if (!checkEmail(email)) throw new Error ("Email address provided is invalid");
-                if (password.length < 8) throw new Error ("Password must be at least 8 characters");
-                if (password != confirmPassword) throw new Error ("Passwords do not match");
-                if (!states.includes(state.toUpperCase())) throw new Error ("State must be entered in 2 letter format");
-                if (!zipRE.test(zip)) throw new Error ("Zip code must be entered in 5 digit format");
-                if (!phoneRE.test(phone)) throw new Error ("Phone number must be entered in 10 digit format");
-                
+                businessSignUpFormErrorCheck(businessName, email, password, confirmPassword, address, city, state, zip, phoneNumber, about);
             } catch (e) {
                 event.preventDefault();
-                let errorMsg = e.toString().slice(7);
-                errorDiv.innerHTML = errorMsg;
+                errorDiv.innerHTML = e;
                 errorDiv.hidden = false;
             }
             
